d8f4c87db611245a345b0482ef64250d2d0d0722
lhch12 <lhc12@outlook.com>
2016-01-17 02:44:40 +0000

    Corrected index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var express = require('express')
-var app = express()
-var fs = require('fs')
+var express = require('express');
+var app = express();
+var fs = require('fs');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('./index.html').toString();)
-})
+  response.send(fs.readFileSync('./index.html').toString());
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
